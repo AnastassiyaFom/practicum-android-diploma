519a491283237ba841c5690fd5e9fd771e0b6b63
lizaKotsar <yelizaveta.kotsar@bk.ru>
2025-12-20 22:27:14 +0300

    Добавила заглушки для экранов избранное, команда, вакансия

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/team/TeamFragment.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.ui.team
+
+import ru.practicum.android.diploma.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class TeamFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_team, container, false)
+    }
+}
