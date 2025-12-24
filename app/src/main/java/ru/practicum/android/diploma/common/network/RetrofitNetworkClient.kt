3968a9d3de257476f23ed3c8c4a06b791975b2fb
SimpleSergei <srglbdev@gmail.com>
2025-12-24 23:54:14 +0700

    Настройка Koin, мелкие правки в проекте.

--- app/src/main/java/ru/practicum/android/diploma/common/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/network/RetrofitNetworkClient.kt
@@ -19,5 +19,6 @@ class RetrofitNetworkClient(private val context: Context, private val vacancyApi
         const val SUCCESS_CODE = 200
         const val BAD_REQUEST_CODE = 400
         const val SERVER_ERROR_CODE = 500
+        const val NOT_FOUND_CODE = 404
     }
 }
