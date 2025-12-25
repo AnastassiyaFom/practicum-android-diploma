1c280cd9a53c1bb812c44aed0abf2cb931d1c8fd
AnastassiyaFom <a.a.fomenkova@mail.ru>
2025-12-25 16:46:56 +0300

    Настроила БД Создала интерфейсы репозитория и интерактора для работы с БД избранных треков, имплементировала их

---
+++ app/src/main/java/ru/practicum/android/diploma/favorites/data/db/VacancyDbConverter.kt
@@ -0,0 +1,72 @@
+package ru.practicum.android.diploma.favorites.data.db
+
+import ru.practicum.android.diploma.search.domain.models.Vacancy
+
+class VacancyDbConverter {
+    fun map(vacancy: Vacancy): VacancyEntity {
+        val phonesString = vacancy.phone?.joinToString(separator = ",")
+        val skillsString = vacancy.skills?.joinToString(separator = ",")
+        return VacancyEntity(
+            id = vacancy.id,
+            name = vacancy.name,
+            description = vacancy.description,
+            experience = vacancy.experience,
+            schedule = vacancy.schedule,
+            employment = vacancy.employment,
+            areaName = vacancy.areaName,
+            industryName = vacancy.industryName,
+            skills = skillsString,
+            url = vacancy.url,
+            // Поля зарплаты
+            salaryFrom = vacancy.salaryFrom,
+            salaryTo = vacancy.salaryTo,
+            currency = vacancy.currency,
+            // Поля адреса
+            city = vacancy.city,
+            street = vacancy.street,
+            building = vacancy.building,
+            fullAddress = vacancy.fullAddress,
+            // Контакты
+            contactName = vacancy.contactName,
+            email = vacancy.email,
+            phone = phonesString,
+            // Работодатель
+            employerName = vacancy.employerName,
+            logoUrl = vacancy.logoUrl
+        )
+    }
+
+    fun map(vacancy: VacancyEntity?): Vacancy {
+        if (vacancy==null) return Vacancy()
+        val phones: List<String> = vacancy.phone?.split(",") ?: emptyList()
+        val skills: List<String> = vacancy.skills?.split(",") ?: emptyList()
+        return Vacancy(
+            id = vacancy.id,
+            name = vacancy.name,
+            description = vacancy.description,
+            experience = vacancy.experience,
+            schedule = vacancy.schedule,
+            employment = vacancy.employment,
+            areaName = vacancy.areaName,
+            industryName = vacancy.industryName,
+            skills = skills,
+            url = vacancy.url,
+            // Поля зарплаты
+            salaryFrom = vacancy.salaryFrom,
+            salaryTo = vacancy.salaryTo,
+            currency = vacancy.currency,
+            // Поля адреса
+            city = vacancy.city,
+            street = vacancy.street,
+            building = vacancy.building,
+            fullAddress = vacancy.fullAddress,
+            // Контакты
+            contactName = vacancy.contactName,
+            email = vacancy.email,
+            phone = phones,
+            // Работодатель
+            employerName = vacancy.employerName,
+            logoUrl = vacancy.logoUrl
+        )
+    }
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/impl/FavoritesVacanciesInteractorImpl.kt
@@ -0,0 +1,26 @@
+package ru.practicum.android.diploma.favorites.domain.impl
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.favorites.domain.api.FavoritesVacanciesInteractor
+import ru.practicum.android.diploma.favorites.domain.api.FavoritesVacanciesRepository
+import ru.practicum.android.diploma.search.domain.models.Vacancy
+
+class FavoritesVacanciesInteractorImpl(
+    private val favoritesVacanciesRepository: FavoritesVacanciesRepository
+): FavoritesVacanciesInteractor {
+    override fun addVacancyToFavorites(vacancy: Vacancy) {
+        favoritesVacanciesRepository.addVacancyToFavorites(vacancy)
+    }
+
+    override fun deleteVacancyFromFavorites(vacancyId: String) {
+        favoritesVacanciesRepository.deleteVacancyFromFavorites(vacancyId)
+    }
+
+    override fun getVacancyById(vacancyId: String): Vacancy? {
+        return favoritesVacanciesRepository.getVacancyById(vacancyId)
+    }
+
+    override fun getAllVacanciesFromFavorites(): Flow<List<Vacancy?>> {
+        return favoritesVacanciesRepository.getAllVacanciesFromFavorites()
+    }
+}
