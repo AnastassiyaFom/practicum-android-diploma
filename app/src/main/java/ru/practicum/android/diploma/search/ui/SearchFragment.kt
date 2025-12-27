442d4c72233205525841dc471bce4d77bc740ce6
vitalkava <vitalikovalevich@gmail.com>
2025-12-28 00:36:15 +0300

    clear icon behavior

--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -2,6 +2,9 @@ package ru.practicum.android.diploma.search.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
@@ -11,7 +14,24 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val searchInput = view.findViewById<EditText>(R.id.search_input)
+        val drawableEnd = view.findViewById<ImageView>(R.id.drawable_end)
         val filterButton = view.findViewById<View>(R.id.btn_filter)
+
+        searchInput.addTextChangedListener { text ->
+            val isEmpty = text.isNullOrEmpty()
+            drawableEnd.setImageResource(
+                if (isEmpty) R.drawable.ic_search else R.drawable.ic_clear
+            )
+        }
+
+        drawableEnd.setOnClickListener {
+
+            if (!searchInput.text.isNullOrEmpty()) {
+                searchInput.text?.clear()
+            }
+        }
+
         filterButton.setOnClickListener {
             findNavController().navigate(R.id.action_searchFragment_to_filterFragment)
         }
