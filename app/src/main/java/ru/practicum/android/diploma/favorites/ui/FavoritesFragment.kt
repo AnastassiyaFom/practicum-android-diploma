47d29d1f2879dbe73536e9f89e2ae73181e2f2ec
AnastassiyaFom <a.a.fomenkova@mail.ru>
2025-12-28 18:21:30 +0300

    исправление замечаний гита

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoritesFragment.kt
@@ -26,7 +26,6 @@ class FavoritesFragment : Fragment() {
             toVacancyDetail(vacancy.id)
         }
 
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
