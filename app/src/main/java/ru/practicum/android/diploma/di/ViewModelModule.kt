5741a3de9e0a5118d9eb3100dfdc125695e563cb
AnastassiyaFom <a.a.fomenkova@mail.ru>
2025-12-30 16:23:17 +0300

    сделала функцию showPlaceholder

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,13 +3,12 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.favorites.ui.FavoritesViewModel
+import ru.practicum.android.diploma.search.ui.SearchViewModel
 
 val viewModelModule = module {
     factory {
         FavoritesViewModel(get(), get())
     }
-import ru.practicum.android.diploma.search.ui.SearchViewModel
 
-val viewModelModule = module {
     viewModel { SearchViewModel(get()) }
 }
