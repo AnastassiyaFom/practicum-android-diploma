c09baaeb5d0537463d6a6b0893d67db0c19fbaa9
AnastassiyaFom <a.a.fomenkova@mail.ru>
2026-01-11 02:35:52 +0300

    исправление merge conflict

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,6 +3,8 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.favorites.ui.FavoritesViewModel
+import ru.practicum.android.diploma.filters.ui.CountryViewModel
+import ru.practicum.android.diploma.filters.ui.PlaceOfWorkViewModel
 import ru.practicum.android.diploma.filters.ui.presentation.FilterViewModel
 import ru.practicum.android.diploma.search.ui.SearchViewModel
 import ru.practicum.android.diploma.vacancy.ui.VacancyViewModel
@@ -18,5 +20,10 @@ val viewModelModule = module {
     viewModel {
         FilterViewModel(get())
     }
-
+    viewModel {
+        CountryViewModel(get(), get())
+    }
+    viewModel {
+        PlaceOfWorkViewModel(get())
+    }
 }
